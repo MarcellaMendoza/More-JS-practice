61a688347513ab556c6250bb420a54f32d640ccb
MarcellaMendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-13 03:19:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ let errorElement = document.getElementById("errorHTML")
+        
+
+function errorJS() {
+  errorElement.innerText = "Something went terribly wrong. Please try again."
+}
+
+
+    
